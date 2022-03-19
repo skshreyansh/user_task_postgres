2eb62343dbf7443ffb5f6294fe1ac20327f04524
Shreyansh <skshreyansh1@gmail.com>
2022-03-19 11:10:20 +0530

    user tatsk postgres

---
+++ handlers/handlers.go
@@ -0,0 +1,335 @@
+package handlers
+
+import (
+	// Includes all packages to be used in this file
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	// An HTTP router
+	// For getting the env variables
+	_ "github.com/lib/pq" // Postgres driver for database/sql, _ indicates it won't be referenced directly in code
+)
+
+// Constants for database, can be set in .env file
+const (
+	// host = "localhost"
+	// port = 5432 // default port for PostgreSQL
+	host     = "localhost"
+	port     = 5432
+	user     = "MASKED"
+	password = "MASKED"
+	dbname   = "postgres"
+)
+
+// The struct for a task, excluding the user_uuid which is added separately.
+// Tasks in JSON will use the JSON tags like "id" instead of "TaskNum".
+type Item struct {
+	TaskNum int    `json:"id"`
+	Task    string `json:"task"`
+	Status  bool   `json:"status"`
+}
+
+// Connect to PostgreSQL database and also retrieve user_id from users table
+func OpenConnection() (*sql.DB, string) {
+	// Getting constants from .env
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+
+	// user, ok := os.LookupEnv("USER")
+	// if !ok {
+	// 	log.Fatal("Error loading env variables")
+	// }
+	// password, ok := os.LookupEnv("PASSWORD")
+	// if !ok {
+	// 	log.Fatal("Error loading env variables")
+	// }
+	// dbname, ok := os.LookupEnv("DB_NAME")
+	// if !ok {
+	// 	log.Fatal("Error loading env variables")
+	// }
+
+	// connecting to database
+	// 1. creating the connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// 2. validates the arguments provided, doesn't create connection to database
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	// 3. actually opens connection to database
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	// add email to users table if not present
+	email := GetEmail()
+	addEmail := `INSERT INTO users (email) VALUES ($1) ON CONFLICT (email) DO NOTHING;`
+	_, err = db.Exec(addEmail, email)
+	if err != nil {
+		panic(err)
+	}
+
+	// get user_id
+	var userId string
+	getUser := `SELECT user_id FROM users WHERE email = $1;`
+	err = db.QueryRow(getUser).Scan(&userId)
+	if err != nil {
+		panic(err)
+	}
+
+	return db, userId
+}
+
+func GetEmail() string {
+	// To be explained, related to authentication
+	return "TEST"
+}
+
+// Get complete list of tasks
+func GetList(w http.ResponseWriter, r *http.Request) {
+	// Set header to json content, otherwise data appear as plain text
+	w.Header().Set("Content-Type", "application/json")
+
+	// Connect to database and get user_id
+	db, userId := OpenConnection()
+
+	// Return all tasks (rows) as id, task, status where the user_uuid of the task is the same as user_id we have obtained in the previous step
+	rows, err := db.Query("SELECT id, task, status FROM tasks JOIN users ON tasks.user_uuid = users.user_id WHERE user_id = $1;", userId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+	defer rows.Close()
+	defer db.Close()
+
+	// Initializing slice like this and not "var items []Item" because aforementioned method returns null when empty thus leading to errors,
+	// while used method returns empty slice
+	items := make([]Item, 0)
+	// Add each task to array of Items
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.TaskNum, &item.Task, &item.Status)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			panic(err)
+		}
+		items = append(items, item)
+	}
+
+	json.NewEncoder(w).Encode(items)
+	// Output with indentation
+	// convert items into byte stream
+	// itemBytes, _ := json.MarshalIndent(items, "", "\t")
+	// // write to w
+	// _, err = w.Write(itemBytes)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusNotFound)
+	// 	panic(err)
+	// }
+
+	// w.WriteHeader(http.StatusOK)
+
+	// Alternatively, output without indentation
+	// NewEncoder: WHERE should the encoder write to
+	// Encode: encode WHAT
+	// _ = json.NewEncoder(w).Encode(items)
+}
+
+func AddTask(w http.ResponseWriter, r *http.Request) {
+	// Set header to json content, otherwise data appear as plain text
+	w.Header().Set("Content-Type", "application/json")
+
+	// decode the requested data to 'newTask'
+	var newTask Item
+
+	// NewDecoder: Decode FROM WHERE
+	// Decode: WHERE TO STORE the decoded data
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	db, userId := OpenConnection()
+	defer db.Close()
+
+	sqlStatement := `INSERT INTO tasks (task, status, user_uuid) VALUES ($1, $2, $3) RETURNING id, task, status;`
+
+	// retrieve the task after creation from the database and store its details in 'updatedTask'
+	var updatedTask Item
+	err = db.QueryRow(sqlStatement, newTask.Task, newTask.Status, userId).Scan(&updatedTask.TaskNum, &updatedTask.Task, &updatedTask.Status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	// gives the new task as the output
+	_ = json.NewEncoder(w).Encode(updatedTask)
+}
+
+// delete task
+var DeleteTask = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Set header to json content, otherwise data appear as plain text
+	w.Header().Set("Content-Type", "application/json")
+
+	// getting the task id from the request URL
+	vars := mux.Vars(r) // vars includes all variables in the request URL route.
+	// For example, in "/list/delete/{id}", "id" is a variable (of type string)
+
+	number, err := strconv.Atoi(vars["id"]) // convert the string id to integer and assign it to variable "number"
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	db, userId := OpenConnection()
+	sqlStatement := `DELETE FROM tasks WHERE id = $1 AND user_uuid = $2;`
+
+	// Note that unlike before, we assign a variable instead of _ to the first returned value by db.Exec,
+	// as we need it to confirm that the row was deleted
+	res, err := db.Exec(sqlStatement, number, userId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	// verifying if row was deleted
+	_, err = res.RowsAffected()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	// to get the remaining tasks, same as the GET function
+	rows, err := db.Query("SELECT id, task, status FROM tasks JOIN users ON tasks.user_uuid = users.user_id WHERE user_id = $1;", userId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+	defer rows.Close()
+	defer db.Close()
+
+	// var items []Item
+	items := make([]Item, 0)
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.TaskNum, &item.Task, &item.Status)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			panic(err)
+		}
+		items = append(items, item)
+	}
+
+	// output with indentation
+	// convert items into byte stream
+	itemBytes, _ := json.MarshalIndent(items, "", "\t")
+
+	// write to w
+	_, err = w.Write(itemBytes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+})
+
+// edit task
+var EditTask = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Set header to json content, otherwise data appear as plain text
+	w.Header().Set("Content-Type", "application/json")
+
+	// get the task id from the request url
+	vars := mux.Vars(r)
+	number, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	sqlStatement := `UPDATE tasks SET task = $2 WHERE id = $1 AND user_uuid = $3 RETURNING id, task, status;`
+
+	// decode the requested data to 'newTask'
+	var newTask Item
+
+	// NewDecoder: Decode FROM WHERE
+	// Decode: WHERE TO STORE the decoded data
+	err = json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	db, userId := OpenConnection()
+	defer db.Close()
+
+	// retrieve the task after creation from the database and store its details in 'updatedTask'
+	var updatedTask Item
+	err = db.QueryRow(sqlStatement, number, newTask.Task, userId).Scan(&updatedTask.TaskNum, &updatedTask.Task, &updatedTask.Status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	// gives the new task as the output
+	_ = json.NewEncoder(w).Encode(updatedTask)
+})
+
+// change task status
+var DoneTask = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Set header to json content, otherwise data appear as plain text
+	w.Header().Set("Content-Type", "application/json")
+
+	// get the task id from the request url
+	vars := mux.Vars(r)
+	number, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	// store current status of the task from database
+	var currStatus bool
+
+	// store updated task
+	var updatedTask Item
+
+	sqlStatement1 := `SELECT status FROM tasks WHERE id = $1 AND user_uuid = $2;`
+	sqlStatement2 := `UPDATE tasks SET status = $2 WHERE id = $1 AND user_uuid = $3 RETURNING id, task, status;`
+
+	db, userId := OpenConnection()
+	defer db.Close()
+
+	// getting current status of the task
+	err = db.QueryRow(sqlStatement1, number, userId).Scan(&currStatus)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+
+	// changing the status of the task
+	err = db.QueryRow(sqlStatement2, number, !currStatus, userId).Scan(&updatedTask.TaskNum, &updatedTask.Task, &updatedTask.Status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+
+	// gives the new task as the output
+	_ = json.NewEncoder(w).Encode(updatedTask)
+})
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"example.com/m/handlers"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin"
+	dbname   = "postgres"
+)
+
+func main() {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckErr(err)
+
+	defer db.Close()
+
+	insertStmt := `insert into "employee"("Name","EmpId") values('Tohit',14)`
+	_, e := db.Exec(insertStmt)
+
+	CheckErr(e)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/list", handlers.GetList).Methods("GET")
+	r.HandleFunc("/add", handlers.AddTask).Methods("POST")
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8000"
+	}
+	// handler = r.Handler(r)
+	log.Println("Listening on port " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		fmt.Println("SDFSDF")
+		panic(err)
+	}
+}
