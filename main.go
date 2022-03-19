c4855ed2b74d29d52fd9192a1ba8fb7fdb0b94df
Shreyansh <skshreyansh1@gmail.com>
2022-03-19 11:12:14 +0530

    masked

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
-	password = "admin"
-	dbname   = "postgres"
+	password = "MASKED"
+	dbname   = "MASKED"
 )
 
 func main() {
